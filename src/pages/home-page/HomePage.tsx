b04bdd4142657eadfb4cd4d3bc14b005c544a915
InoProGit <89451538+InoProGit@users.noreply.github.com>
2023-12-03 19:04:52 +0300

    readme and homepage updated

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -16,15 +16,6 @@ function HomePage() {
               <br/> &#x2022; Tailwind,
               <br/> &#x2022; TypeScript.
             </p>
-
-          <h2 className="text-lg">Contribute</h2>
-            <p className="p-4">
-              To run it locally on your own computer:
-              <br/> &#x2022; [Fork](https://help.github.com/articles/fork-a-repo/) this repository
-              <br/> &#x2022; [Clone](https://help.github.com/articles/cloning-a-repository/) it to your local device
-              <br/> &#x2022; [Install](https://yarnpkg.com/en/docs/cli/install) the dependencies
-              <br/> &#x2022; [Run](https://github.com/bukinoshita/taskr/blob/master/package.json#L25) the project  
-            </p>
           </div>
       </div>
       <br />
